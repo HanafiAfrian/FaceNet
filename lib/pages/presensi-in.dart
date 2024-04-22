ea93dc8a286a0fdf030ab2918979ed23d6d3413b
HanafiAfrian <136046020+HanafiAfrian@users.noreply.github.com>
2024-04-22 14:03:59 +0700

    Update presensi-in deteksi wajah otomatis dan looping ototmatis.dart

--- lib/pages/presensi-in.dart
+++ lib/pages/presensi-in.dart
@@ -1,4 +1,8 @@
 import 'dart:async';
+import 'dart:math';
+import 'package:face_net_authentication/constants/colors.dart';
+import 'package:face_net_authentication/pages/widgets/face_preview.dart';
+import 'package:flutter/material.dart';
 import 'package:face_net_authentication/locator.dart';
 import 'package:face_net_authentication/pages/models/user.model.dart';
 import 'package:face_net_authentication/pages/widgets/auth_button.dart';
@@ -10,7 +14,11 @@ import 'package:face_net_authentication/services/camera.service.dart';
 import 'package:face_net_authentication/services/ml_service.dart';
 import 'package:face_net_authentication/services/face_detector_service.dart';
 import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../constants/colors.dart';
+import 'widgets/face_preview.dart';
 
 class PresensiIn extends StatefulWidget {
   const PresensiIn({Key? key}) : super(key: key);
@@ -28,11 +36,19 @@ class PresensiInState extends State<PresensiIn> {
 
   bool _isPictureTaken = false;
   bool _isInitializing = false;
-
+  bool _isTakingPicture = false; // Added
+  bool cekwajah = true;
+  String? wajah;
   @override
   void initState() {
     super.initState();
+    getPref();
     _start();
+  }
+
+  getPref() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    wajah = prefs.getString('gambarwajah');
   }
 
   @override
@@ -66,6 +82,8 @@ class PresensiInState extends State<PresensiIn> {
     await _faceDetectorService.detectFacesFromImage(image!);
     if (_faceDetectorService.faceDetected) {
       _mlService.setCurrentPrediction(image, _faceDetectorService.faces[0]);
+      // Panggil takePictureAutomatically() setiap kali wajah terdeteksi.
+      await takePictureAutomatically();
     }
     if (mounted) setState(() {});
   }
@@ -101,25 +119,64 @@ class PresensiInState extends State<PresensiIn> {
     }
   }
 
+  Future<void> takePictureAutomatically() async {
+    setState(() => _isTakingPicture = true); // Added
+    if (_faceDetectorService.faceDetected) {
+      await takePicture();
+      User? user = await _mlService.predict();
+      // Evaluasi ekspresi dan cetak hasil
+      double randomNumber = Random().nextDouble();
+      bool result = randomNumber > 0.1;
+      print("Nilai Random(): $randomNumber");
+      print("Hasil evaluasi Random().nextDouble() > 0.1: $result");
+      // Menggunakan hasil evaluasi untuk menentukan tindakan selanjutnya
+      if (result) {
+        if (user != null) {
+          var bottomSheetController = scaffoldKey.currentState!
+              .showBottomSheet((context) => presensiInSheet(user: user));
+          bottomSheetController.closed.whenComplete(_reload);
+        } else {
+          _start(); // Mulai kembali deteksi wajah
+        }
+      } else {
+        _start(); // Mulai kembali deteksi wajah
+      }
+    }
+    setState(() => _isTakingPicture = false); // Added
+  }
+
   Widget getBodyWidget() {
     if (_isInitializing) return Center(child: CircularProgressIndicator());
-    if (_isPictureTaken)
+    if (_isPictureTaken && _cameraService.imagePath != null)
       return SinglePicture(imagePath: _cameraService.imagePath!);
+    if (wajah != null) {
+      setState(() {
+        cekwajah = false;
+      });
+      return FacePreview();
+    }
+
     return CameraDetectionPreview();
   }
 
   @override
   Widget build(BuildContext context) {
-    Widget header =
-        CameraHeader("PRESENSI MASUK", onBackPressed: _onBackPressed);
+    Widget header = CameraHeader(cekwajah == true ? "Preview Wajah" : "Login",
+        onBackPressed: _onBackPressed);
     Widget body = getBodyWidget();
     Widget? fab;
-    if (!_isPictureTaken) fab = AuthButton(onTap: onTap);
+    // if (!_isPictureTaken) fab = AuthButton(onTap: onTap);
 
     return Scaffold(
       key: scaffoldKey,
-      body: Stack(
-        children: [body, header],
+      body: ModalProgressHUD(
+        inAsyncCall: _isTakingPicture, // Added
+        progressIndicator: CircularProgressIndicator(
+          color: primaryColor,
+        ), // Added
+        child: Stack(
+          children: [body, header],
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: fab,
