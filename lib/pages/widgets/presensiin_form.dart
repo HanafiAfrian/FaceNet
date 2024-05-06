14119e8a78aa18f7f300dfd496b8f208c02adaae
HanafiAfrian <136046020+HanafiAfrian@users.noreply.github.com>
2024-05-06 12:46:41 +0700

    Update presensiin_form Menghilangkan Password

--- lib/pages/widgets/presensiin_form.dart
+++ lib/pages/widgets/presensiin_form.dart
@@ -25,7 +25,6 @@ class PresensiInSheet extends StatefulWidget {
 }
 
 class _PresensiInSheetState extends State<PresensiInSheet> {
-  final _passwordController = TextEditingController();
   final _cameraService = locator<CameraService>();
   String? _latitude;
   String? _longitude;
@@ -197,79 +196,69 @@ class _PresensiInSheetState extends State<PresensiInSheet> {
           );
         },
       );
-    } else if (user.password == _passwordController.text) {
-      try {
-        final response = await http.post(
-          Uri.parse(Constants.BASEURL + Constants.ABSENSIMASUK),
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          body: {
-            'nip': user.nip,
-            'nama': user.user,
-            'jenis_absensi': 'Masuk',
-            'latitude': _latitude.toString() ?? '',
-            'longitude': _longitude.toString() ?? '',
-          },
-        );
+    }
+    try {
+      final response = await http.post(
+        Uri.parse(Constants.BASEURL + Constants.ABSENSIMASUK),
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: {
+          'nip': user.nip,
+          'nama': user.user,
+          'jenis_absensi': 'Masuk',
+          'latitude': _latitude.toString() ?? '',
+          'longitude': _longitude.toString() ?? '',
+        },
+      );
 
-        if (response.statusCode == 200) {
-          final trimmedResponse = response.body.trim().toLowerCase();
-          if (trimmedResponse == 'false') {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  content: Text('Anda berada di luar area presensi.'),
-                );
-              },
-            );
-          } else if (trimmedResponse == 'true') {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (BuildContext context) => MainScreen(
-                  username: user.nip,
-                  imagePath: _cameraService.imagePath!,
-                ),
+      if (response.statusCode == 200) {
+        final trimmedResponse = response.body.trim().toLowerCase();
+        if (trimmedResponse == 'false') {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: Text('Anda berada di luar area presensi.'),
+              );
+            },
+          );
+        } else if (trimmedResponse == 'true') {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) => MainScreen(
+                username: user.nip,
+                imagePath: _cameraService.imagePath!,
               ),
-            );
-          } else {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  content: Text('Invalid server response.'),
-                );
-              },
-            );
-          }
+            ),
+          );
         } else {
           showDialog(
             context: context,
             builder: (context) {
               return AlertDialog(
-                content: Text('Failed to connect to the server.'),
+                content: Text('Invalid server response.'),
               );
             },
           );
         }
-      } catch (error) {
+      } else {
         showDialog(
           context: context,
           builder: (context) {
             return AlertDialog(
-              content: Text('Error: $error'),
+              content: Text('Failed to connect to the server.'),
             );
           },
         );
       }
-    } else {
+    } catch (error) {
       showDialog(
         context: context,
         builder: (context) {
           return AlertDialog(
-            content: Text('Wrong password!'),
+            content: Text('Error: $error'),
           );
         },
       );
@@ -294,12 +283,6 @@ class _PresensiInSheetState extends State<PresensiInSheet> {
                 Container(
                   child: Column(
                     children: [
-                      SizedBox(height: 10),
-                      AppTextField(
-                        controller: _passwordController,
-                        labelText: "Password",
-                        isPassword: true,
-                      ),
                       SizedBox(height: 10),
                       Divider(),
                       SizedBox(height: 10),
